7f64b8522d47826790703412e36afa008e5aeb13
blackplume <qingyu.s@foxmail.com>
2018-06-25 12:10:49 +0800

    修改持续化集成服务

--- SUMMARY.md
+++ SUMMARY.md
@@ -2,4 +2,5 @@
 
 * [Introduction](README.md)
 * [游戏数值设计](gamenumericaldesign.md)
+* [关于游戏教育意义的思考](educational.md)
 
---
+++ deploy.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -o errexit -o nounset
+
+if [ "$TRAVIS_BRANCH" != "master" ]
+then 
+    echo "This commit was made against the $TRAVIS_BRANCH and not the master! No deploy!" 
+    exit 0
+fi
+
+rev=$(git rev-parse --short HEAD)
+
+cd _book
+
+git initgit config user.name "blackplume"
+
+git config user.email "qingyu.s@foxmail.com"
+
+git remote add upstream "https://github.com/blackplume233/development-of-games.git"
+
+git fetch upstream
+
+git reset upstream/master
+
+echo "blackplume" > CNAME
+
+git add -A
+
+git commit -m "rebuild pages at ${rev}"
+
+git push -q upstream HEAD:master
